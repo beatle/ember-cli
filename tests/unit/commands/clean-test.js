ef7569d046eb9963b4d2d65ccfa03bc6fbf3efbf
beatle <kudgo.test@gmail.com>
2015-10-10 03:24:46 +0300

    npm tasks tests

--- tests/unit/commands/clean-test.js
+++ tests/unit/commands/clean-test.js
@@ -1,13 +1,12 @@
 'use strict';
 
-var expect         = require('chai').expect;
-var stub           = require('../../helpers/stub').stub;
-var MockProject    = require('../../helpers/mock-project');
-var commandOptions = require('../../factories/command-options');
-var Task           = require('../../../lib/models/task');
-var Promise        = require('../../../lib/ext/promise');
-// var AddonInstall   = require('../../../lib/tasks/addon-install');
-var CleanCommand = require('../../../lib/commands/clean');
+var expect          = require('chai').expect;
+var stub            = require('../../helpers/stub').stub;
+var MockProject     = require('../../helpers/mock-project');
+var commandOptions  = require('../../factories/command-options');
+var Task            = require('../../../lib/models/task');
+var Promise         = require('../../../lib/ext/promise');
+var CleanCommand    = require('../../../lib/commands/clean');
 
 describe('install command', function() {
   var npmInstance, bowerInstance, removeFilesInstance;
